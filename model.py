ff3ee6375fa8c2cd025f231e31a7f0c28f1ffb06
Fr1edly <82762414+Fr1edly@users.noreply.github.com>
2024-04-17 18:07:44 +0700

    modify model to be able to process 2 images at once

--- model.py
+++ model.py
@@ -2,6 +2,25 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 
 def create_model():
+    input_shape = (28, 28, 1)
+
+    input_img1 = Input(shape=input_shape)
+    input_img2 = Input(shape=input_shape)
+
+    processed_img1 = Conv2D(32, (3, 3), activation='relu')(input_img1)
+    processed_img1 = MaxPooling2D((2, 2))(processed_img1)
+    processed_img1 = Flatten()(processed_img1)
+
+    processed_img2 = Conv2D(32, (3, 3), activation='relu')(input_img2)
+    processed_img2 = MaxPooling2D((2, 2))(processed_img2)
+    processed_img2 = Flatten()(processed_img2)
+
+    merged = concatenate([processed_img1, processed_img2])
+
+    prediction = Dense(100, activation='softmax')(merged)
+
+    model = Model(inputs=[input_img1, input_img2], outputs=prediction)
+    '''
     model = Sequential([
         Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
         MaxPooling2D(pool_size=(2, 2)),
@@ -11,6 +30,7 @@ def create_model():
         Dense(128, activation='relu'),
         Dense(10, activation='softmax')
     ])
+    '''
 
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
