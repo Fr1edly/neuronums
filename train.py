2351eb26c0bc69a8605727cafa5fb75c4381d6f6
Fr1edly <82762414+Fr1edly@users.noreply.github.com>
2024-04-17 18:40:59 +0700

    tune train function

--- train.py
+++ train.py
@@ -2,17 +2,22 @@ from model import create_model
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 
-
 def load_data():
+    # Загрузка данных MNIST
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255
-    x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255
-    y_train = to_categorical(y_train, 10)
-    y_test = to_categorical(y_test, 10)
+
+    # Подготовка данных для двух входов модели
+    x_train = [x_train.reshape(-1, 28, 28, 1).astype('float32') / 255] * 2
+    x_test = [x_test.reshape(-1, 28, 28, 1).astype('float32') / 255] * 2
+
+    # Преобразование меток в one-hot кодировку
+    y_train = to_categorical(y_train, 100)
+    y_test = to_categorical(y_test, 100)
+
     return x_train, y_train, x_test, y_test
 
-
 def train_model(model, x_train, y_train, x_test, y_test):
+    # Обучение модели с двумя входами
     model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5, batch_size=128)
 
 
@@ -20,4 +25,4 @@ if __name__ == "__main__":
     x_train, y_train, x_test, y_test = load_data()
     model = create_model()
     train_model(model, x_train, y_train, x_test, y_test)
-    model.save('mnist_cnn.h5')
+    model.save('./model/mnist_cnn.h5')
